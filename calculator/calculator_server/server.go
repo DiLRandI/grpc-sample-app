5e90d593d5cbea8238b41ed49d0ba6872e27d2e4
Deleema Fernando <DFernando@ieDigital.com>
2019-06-16 10:23:40 +0530

    calculator server

---
+++ calculator/calculator_server/server.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"net"
+
+	"github.com/dilrandi/grpc-sample-app/calculator/calculatorpb"
+	"google.golang.org/grpc"
+)
+
+type server struct{}
+
+func (*server) Sum(ctx context.Context, r *calculatorpb.CalcRequest) (*calculatorpb.CalcResponse, error) {
+	res := &calculatorpb.CalcResponse{
+		Result: r.No1 + r.No2,
+	}
+	return res, nil
+}
+func main() {
+	fmt.Println("Calculator server starting")
+	lis, err := net.Listen("tcp", "0.0.0.0:50051")
+
+	if err != nil {
+		panic(err)
+	}
+
+	s := grpc.NewServer()
+	calculatorpb.RegisterCalculatorServiceServer(s, &server{})
+
+	if err = s.Serve(lis); err != nil {
+		panic(err)
+	}
+}
