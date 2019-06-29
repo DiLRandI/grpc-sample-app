85000020e0c176c1cc62784acf2a5262b95d259f
Deleema Fernando <DFernando@ieDigital.com>
2019-06-29 13:19:55 +0530

    Test monog env

--- blog/blog_server/server.go
+++ blog/blog_server/server.go
@@ -1,24 +1,59 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net"
 	"os"
 	"os/signal"
+	"time"
 
 	"github.com/dilrandi/grpc-sample-app/blog/blogpb"
-
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"google.golang.org/grpc"
 )
 
 type blogServer struct {
+}
+type Person struct {
+	Fname string
+	Lname string
+	Age   int
 }
 
 func main() {
 	// if we crash, will get files name and line number
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
 	fmt.Println("Blog server ... !")
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://root:example@localhost:27017"))
+
+	if err != nil {
+		log.Fatalf("Error while creating mongo client : %v", err)
+	}
+
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err = client.Connect(ctx)
+
+	if err != nil {
+		log.Fatalf("Error while connecting to mongo server : %v", err)
+	}
+
+	res, err := client.Database("deleema").Collection("HelloWorld").InsertOne(ctx,
+		&Person{
+			Age:   28,
+			Fname: "Deleema",
+			Lname: "Fernando",
+		})
+
+	if err != nil {
+		log.Fatalf("Errorrr : %v", err)
+	}
+
+	fmt.Println("=======================================================================")
+	fmt.Printf("\n\n %+v \n\n", res)
+	fmt.Println("=======================================================================")
 
 	lis, err := net.Listen("tcp", "0.0.0.0:5051")
 	if err != nil {
